5a902ddeed58cc3fd37ad60b208a8ce52de6eb50
cotn <q003772003@gmail.com>
2023-07-31 16:20:29 +0800

    新增圖片middlewares

---
+++ middlewares/upload.js
@@ -0,0 +1,59 @@
+// 解析一種叫 form-data 資料格式的套件
+import multer from 'multer'
+import { v2 as cloudinary } from 'cloudinary'
+// 跟 multer 搭配使用，用來將資料放到 cloudinary
+import { CloudinaryStorage } from 'multer-storage-cloudinary'
+import { StatusCodes } from 'http-status-codes'
+
+// cloudinary 配置
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET
+})
+
+const upload = multer({
+  // 設定儲存位置
+  storage: new CloudinaryStorage({ cloudinary }),
+  // 過濾前端送過來的檔案
+  fileFilter (req, file, callback) {
+    // callback(錯誤, 是否允許)
+    if (['image/jpg', 'image/jpeg', 'image/png'].includes(file.mimetype)) {
+      callback(null, true)
+    } else {
+      // 使用跟套件一樣的錯誤格式，方便之後處理
+      callback(new multer.MulterError('LIMIT_FILE_FORMAT'), false)
+    }
+  },
+  limits: {
+    // 限制檔案大小，超過會出現 LIMIT_FILE_SIZE 錯誤
+    fileSize: 1024 * 1024
+  }
+})
+
+export default (req, res, next) => {
+  upload.single('image')(req, res, error => {
+    if (error instanceof multer.MulterError) {
+      // 處理上傳錯誤
+      let message = '上傳錯誤'
+      if (error.code === 'LIMIT_FILE_SIZE') {
+        message = '檔案太大'
+      } else if (error.code === 'LIMIT_FILE_FORMAT') {
+        message = '檔案格式錯誤'
+      }
+      res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message
+      })
+    } else if (error) {
+      // 處理其他錯誤
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: '伺服器錯誤'
+      })
+    } else {
+      // 沒有錯誤，繼續
+      next()
+    }
+  })
+}
