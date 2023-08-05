c465b54f5d7ab033229567af39a21887920c6418
chanyenlee <q003772003@gmail.com>
2023-08-05 13:14:31 +0800

    controllers getALLl修改成可以查詢作者、類別、作品名稱

--- controllers/images.js
+++ controllers/images.js
@@ -35,8 +35,15 @@ export const create = async (req, res) => {
 export const getAll = async (req, res) => {
   try {
     // .find( mongoDB 資料庫的 key : key 的 value )
-    // 下面那行.find裡面的 new RegExp() 是正則表達式的語法，console.log()留在下面可以看
-    const result = await images.find({ theme: new RegExp(req.query.search, 'i') })
+    const result = await images.find({
+      // $or:[] 這個是其中一個的語法，要是陣列，裡面物件表示
+      $or: [
+        // new RegExp() 是正則表達式的語法，console.log()留在下面可以看
+        { theme: new RegExp(req.query.search, 'i') },
+        { userName: new RegExp(req.query.search, 'i') },
+        { name: new RegExp(req.query.search, 'i') }
+      ]
+    })
     // console.log({ name: new RegExp(req.query.search, 'i') })
     res.status(StatusCodes.OK).json({
       success: true,
